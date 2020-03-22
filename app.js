662e4603e56f8b10dab36c2ce0053a3078e86551
Saksham <sakshamkhatwani@gmail.com>
2020-03-23 01:44:34 +0530

    connected DB and made models folder

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var mongoDB = 'mongodb+srv://saksham:SamSham@46@cluster0-a5hnz.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
